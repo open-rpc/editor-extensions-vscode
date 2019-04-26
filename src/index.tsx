b2d63f9e07dc9ef2afe1b2fd1d5ad57fa1f282fb
Vijay <Sharmaa76Vijay@gmail.com>
2019-04-26 12:48:07 +0530

    feat: add entrypoint files for react-scripts

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Basic React Application</h1>,
+  document.getElementById('root') as HTMLElement
+);
