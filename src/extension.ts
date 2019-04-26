1388318fd5d8e826db4fabc67facc1e8d88d7aa6
Vijay <Sharmaa76Vijay@gmail.com>
2019-04-26 11:57:10 +0530

    feat: add command to open basic webview panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import createOrRevealWebView from './ReactWebview';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,6 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 		console.log('Congratulations, your extension "open-rpc-meta-schema" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('openrpc.previewDoc', () => {
+			createOrRevealWebView();
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
