93345a2b648b25a72f08607338a372c3acb67157
Vijay <Sharmaa76Vijay@gmail.com>
2019-04-29 16:16:43 +0530

    fix: separate folder for extension code base

--- extension/extension.ts
+++ extension/extension.ts
@@ -6,13 +6,10 @@ import createOrRevealWebView from './ReactWebview';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "open-rpc-meta-schema" is now active!');
-
 	context.subscriptions.push(
 		vscode.commands.registerCommand('openrpc.previewDoc', () => {
-			createOrRevealWebView();
+			// Create and show panel
+			createOrRevealWebView(context);
 		})
 	);
 }
