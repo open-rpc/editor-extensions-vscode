ffbe8842081182bdcad380998bafe5cf9ef42e8c
shanejonas <jonas.shane@gmail.com>
2021-09-23 14:20:10 -0700

    feat: WIP multiple files

--- extension/extension.ts
+++ extension/extension.ts
@@ -6,6 +6,7 @@ import createOrRevealWebView from './ReactWebview';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('openrpc.previewDoc', () => {
       try {
